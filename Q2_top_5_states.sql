c1b15ce95a100c856651fccfa93af895240af7fc
Raghav S <raghvv02@gmail.com>
2025-10-25 14:55:07 +0530

    Delete Q2_top_5_states.sql

--- Q2_top_5_states.sql
+++
@@ -1,9 +0,0 @@
-select
-state,
-(sum(s.electric_vehicles_sold)/sum(s.total_vehicles_sold))*100 as pen_rate
-from electric_vehicle_sales_by_state s
-left join dim_date d on s.date = d.date
-group by state
-order by pen_rate desc
-limit 5;
-
